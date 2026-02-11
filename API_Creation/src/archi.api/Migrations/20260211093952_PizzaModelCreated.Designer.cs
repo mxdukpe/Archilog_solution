b625480020a50d01033de375d55ad380212ff479
mxdukpe <anais.asse-akakpo@epitech.eu>
2026-02-11 10:45:06 +0100

    feat: Add Pizza and Tacos models with migration support

--- API_Creation/src/archi.api/Migrations/20260211093952_PizzaModelCreated.Designer.cs
+++ API_Creation/src/archi.api/Migrations/20260211093952_PizzaModelCreated.Designer.cs
@@ -3,6 +3,7 @@ using Archi.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace archi.api.Migrations
 {
     [DbContext(typeof(ArchiDbContext))]
-    partial class ArchiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260211093952_PizzaModelCreated")]
+    partial class PizzaModelCreated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API_Creation/src/archi.api/Migrations/20260211093952_PizzaModelCreated.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace archi.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PizzaModelCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pizzas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Base = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsVegetarian = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pizzas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pizzas");
+        }
+    }
+}
